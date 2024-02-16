47dfaf0da160f62b271210bce0d25676bbbf5f8e
Crowdbotics <team@crowdbotics.com>
2024-02-16 16:28:17 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pagepals-46400",
+  url: "https://pagepals-46400.botics.co",
+  api: "https://pagepals-46400.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
